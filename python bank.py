ed1255f1b725d9a056a6f0d92e01c801be04dcc6
Reedo-msr <113295851+Reedo-msr@users.noreply.github.com>
2022-09-14 15:38:10 +0700

    Update python bank.py

--- python bank.py
+++ python bank.py
@@ -1,12 +1,12 @@
 #Editor Muhammad Syifa Ridhoni (220535604679)
 #S1 Teknik Informatika Offering C
 #Tugas Algoritma Pemrograman
-name = input("? ").strip()
+name = input("What do you want to type? (Hello/Hello, Newman/How are you doing?/What's happening?) ").strip()
 
 match name:
     case "Hello"|"Hello, Newman":
         print("$0")
-    case "How you doing?":
+    case "How are you doing?":
         print("20$")
     case "What's happening?":
         print("100")
